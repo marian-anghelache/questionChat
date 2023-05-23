1f8ab77ab5a82bc17825b9ce2355247e7d83cb00
Marian A <69572386+marian-anghelache@users.noreply.github.com>
2023-05-23 18:49:05 +0300

    Update config.edge.ts

--- config.edge.ts
+++ config.edge.ts
@@ -1,7 +1,7 @@
 import type { AppConfig } from "./lib/edge/types.ts";
 
-//import { prompt } from "./prompts/movie-critic.ts";
-import { prompt } from "./prompts/tour-guide.ts";
+import { prompt } from "./prompts/movie-critic.ts";
+//import { prompt } from "./prompts/tour-guide.ts";
 
 export const appConfig: AppConfig = {
   // This should be set in an environment variable
