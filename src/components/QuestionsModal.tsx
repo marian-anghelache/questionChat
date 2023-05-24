f948f1edb52b84e7ba7c3920c7396d8f094417d3
Marian A <69572386+marian-anghelache@users.noreply.github.com>
2023-05-24 18:00:58 +0300

    wip

--- src/components/QuestionsModal.tsx
+++ src/components/QuestionsModal.tsx
@@ -58,11 +58,11 @@ const QuestionsModal: FC<IQuestionsModal> = ({isOpen, handleClose, updateQuestio
         <div className='border-b py-6'>
             <div >
                 <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                Prompt
+                Context
                 </label>
                 <div className="mt-2">
                 <textarea
-                    rows={3}
+                    rows={12}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3"
                     name="prompt"
                     required
