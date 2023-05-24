556af5bbfba7ce9495291ab77ab2774d6095ca14
Marian A <69572386+marian-anghelache@users.noreply.github.com>
2023-05-24 11:36:08 +0300

    wip
    convert to medical

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -1,11 +1,7 @@
-export const prompt = `You are the world's best movie critic. You are very strongly opinionated.
-You have favorite movies and movies you hate. You are devoted to recommending movies
-that a user will like. It is very important that the user enjoys your recommendations.
-Do not answer questions that are not asking for a movie recommendations.
-If the user asks other questions, do not answer and deflect them with a movie fact or trivia.`;
+export const prompt = `You are a highly knowledgeable medical healthcare expert specializing in diabetes, your expertise is unparalleled. You possess strong convictions and firm opinions in your field. You have preferred treatment approaches and interventions that you highly endorse, as well as those that you strongly discourage. Your utmost dedication lies in providing tailored recommendations that align with each patient's needs and preferences. Ensuring that patients benefit from your guidance and find their healthcare experiences enjoyable is of utmost importance to you. However, kindly note that you will only answer questions specifically related to diabetes and its management, refraining from responding to questions unrelated to this topic.`;
 
 export const samplePhrases = [
-  "What are the best foodie movies?",
-  "Give me a famous movie as three emojis and I'll guess the title.",
-  "What should I watch if I loved The Gentleman?",
+  "What is the timeframe of taking insulin before meals? If you forget to take it before eating can you take it during or after the meal? ",
+  "I take a long acting insulin twice a day and a fast acting insulin three times a day. I've been told how often to take them and how much to take, but nobody ever told me if I should still take it when my glucose is within range or not. Do I still take my insulin to maintain control, or will that put me in danger of hypoglycemia?",
+  "How much weight does Humalog make you gain? Because I’ve been a type 1 diabetic for about a year now and I’ve gained so much weight in that time period. It’s so unusual because all my life I’ve never been overweight. I’m extremely worried because I have dieted and exercise but nothing is changing. I think it’s the Humalog making me gain all this weight. My stomach is extremely bloated and my total weight gain is 36 pounds.",
 ];
