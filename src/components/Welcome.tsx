9c88b465cbdaaa3d2aa9600594364fb3637d675e
Marian A <69572386+marian-anghelache@users.noreply.github.com>
2023-05-24 17:42:02 +0300

    wip

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -15,10 +15,7 @@ export const Welcome: React.FC<IWelcome> = ({handleSetQuestionsClick}) => (
       </button>
     </div>
     <h1 className="text-2xl font-bold mb-2">LawAssist - Your trusted virtual legal assistant</h1>
-    <p>We understand that navigating the complexities of the legal world can be overwhelming. That's why we're here to provide you with personalized guidance and support.<br/><br/>
-    Whether you have questions about legal procedures, need assistance with legal documents, or seek general legal advice, our intelligent chatbot is ready to help.<br><br/>
-    Get started now and experience the convenience of having a reliable legal companion at your fingertips.
-    </p>
+    <p>We understand that navigating the complexities of the legal world can be overwhelming. That's why we're here to provide you with personalized guidance and support. Whether you have questions about legal procedures, need assistance with legal documents, or seek general legal advice, our intelligent chatbot is ready to help.</p>
     <p>
     </p>
   </div>
