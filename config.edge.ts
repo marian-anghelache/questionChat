02fae9f0218fba9786ea04cefa1d417bab536aeb
Marian A <69572386+marian-anghelache@users.noreply.github.com>
2023-05-24 17:45:09 +0300

    wip

--- config.edge.ts
+++ config.edge.ts
@@ -28,7 +28,7 @@ export const appConfig: AppConfig = {
   // This can be a plain string if you'd prefer, or you can use
   // information from the request or context to generate it.
   systemPrompt: (_req, context) => `${prompt}
-Respond with valid markdown. Put movie names in bold. Knowledge cutoff September 2021.
+Respond with valid markdown. Put laws in bold. Knowledge cutoff September 2021.
 Current date: ${new Date().toDateString()}.
 User location: ${context.geo.city}, ${context.geo.country}`,
 };
